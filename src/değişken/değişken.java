138bac5bf8c2794775900f529cf536e1323cef7d
acercagri <cagri-acer@outlook.com>
2022-11-08 15:43:03 +0300

    a

---
+++ src/değişken/değişken.java
@@ -0,0 +1,15 @@
+package değişken;
+
+public class değişken {
+
+	public static void main(String[] args) {
+		int musteriSayisi = 15;
+		String koltuk ="musteriSayisi : ";
+		System.out.println(koltuk + musteriSayisi);
+		System.out.println(koltuk + musteriSayisi);
+		System.out.println(koltuk + musteriSayisi);
+		System.out.println(koltuk + musteriSayisi);
+		
+	}
+
+}
